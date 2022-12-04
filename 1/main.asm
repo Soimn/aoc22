58fc5fd9caac82ed19b665869fabc77c547e72fb
Soimn <soimn.dok@gmail.com>
2022-12-04 07:38:17 +0100

    day 1 finished

--- 1/main.asm
+++ 1/main.asm
@@ -9,10 +9,29 @@ _start:
 	mov rdi, [p1input]
 	call read_input_file
 
-	xor r12, r12               ; max_calories
+
+	mov rax, 9     ; sys_mmap
+	mov rdi, 0     ; 0 as address since I don't care where the memory is placed
+	mov rsi, QWORD [input_file_buffer_size]
+	shl rsi, 2     ; rsi = size of allocation, min file size for n elements f=2n-1 => n=(f-1)/2 => 8n=4f-4 < 4f
+	mov rdx, 0x03  ; protection, PROT_READ = 0x1, PROT_WRITE = 
+	mov r10, 0x22  ; flags, MAP_PRIVATE = 0x02, MAP_ANONYMOUS = 0x20
+	mov r8,  -1    ; file descriptor, -1 since some systems may require it to be when MAP_ANONYMOUS is used
+  mov r9,  0     ; offset
+	syscall
+
+	cmp rax, -1
+	jne map_successful
+	mov rdi, failed_to_map_memory
+	call error_and_exit
+	map_successful:
+	sub rsp, 0x10
+	mov QWORD [rsp + 8], rax   ; store scoreboard base first
+	mov QWORD [rsp],     rax   ; scoreboard cursor second
+
 	xor rbx, rbx               ; cursor
 	mov rbp, input_file_buffer ; base
-	main_loop:                 ; while (cursor < input_file_buffer_size)
+	main_loop:                 ; while (cursor < fer_size)
 	cmp rbx, [input_file_buffer_size]
 	jae main_loop_end
 
@@ -20,13 +39,13 @@ _start:
 	cmp rbx, [input_file_buffer_size]
 	jae main_loop_end
 	xor r14, r14
-	mov r15b, BYTE [rbp + rbx]
+	movzx r15, BYTE [rbp + rbx]
 	sub r15, 0x9               ; map ['\t','\r'], which is [0x9,0xD], to [0x0,0x4], this makes the first 5 nonnegative values all whitespace
 	cmp r15, 0x5
 	setb r14b
 	cmp r15, 0x17              ; ' ', which is 0x20, sub 0x9 is 0x17
 	sete r15b
-	or r14, r15
+	or r14b, r15b
 	cmp r14, 0
 	je done_eating_whitespace
 	inc rbx
@@ -41,12 +60,12 @@ _start:
 	cmp r14, 0x0A ; \n
 	je done_counting_calories
 
-	mov r15, rbx ; base pointer for calory string
+	lea r15, [rbp + rbx] ; base pointer for calory string
 
 	eat_number:
 	cmp rbx, [input_file_buffer_size]
 	jae done_eating_number
-	mov r14b, BYTE [rbp + rbx]
+	movzx r14, BYTE [rbp + rbx]
 	cmp r14, 0x0A ; \n
 	je done_eating_number
 	inc rbx
@@ -54,7 +73,7 @@ _start:
 	done_eating_number:
 
 	mov rdi, r15         ; base pointer for calory string
-	mov rsi, rbx
+	lea rsi, [rbp + rbx]
 	sub rsi, r15         ; string length
 	call parse_u64
 	cmp eax, 1
@@ -69,14 +88,35 @@ _start:
 	jmp count_calories
 	done_counting_calories:
 
-	cmp r12, r13
-	cmovb r12, r13 ; max_calories = max(max_calories, calories)
+	mov r12, QWORD [rsp] ; load scoreboard cursor
+	mov QWORD [r12], r13 ; store current calory count in scoreboard
+	add r12, 8           ; |
+	mov QWORD [rsp], r12 ; |> increment scoreboard cursor by u64 size
 
 	jmp main_loop
 	main_loop_end:
 
+	mov r12, QWORD [rsp + 8] ; load scoreboard base pointer
+	mov r13, QWORD [rsp]     ; load scoreboard cursor
+	sub r13, r12             ; |
+	shr r13, 3               ; |> len = (cursor - base)/elem_size
+
 	mov rdi, r12
+	mov rsi, r13
+	call quicksort_u64
+
+	mov rdi, part_1_colon
+	call print
+	mov rdi, QWORD [r12 + r13*8 - 8]
 	call print_u64
+	mov rdi, newline_part_2_colon
+	call print
+	mov rdi, QWORD [r12 + r13*8 - 8]
+	add rdi, QWORD [r12 + r13*8 - 16]
+	add rdi, QWORD [r12 + r13*8 - 24]
+	call print_u64
+	mov rdi, newline
+	call print
 
 	mov rax, 60     ; sys_exit
 	mov rdi, 0      ; exit code
@@ -85,6 +125,12 @@ _start:
 section .data
 	p1in:         db "./p1in.txt",0
 	p1in_example: db "./p1in_example.txt",0
-	p1input:      dq p1in_example
+	p1input:      dq p1in
 
-	failed_to_parse_number: db "Failed to parse number",0
+	failed_to_parse_number: db "Failed to parse number",0xA,0
+	failed_to_map_memory:   db "Failed to map memory",0x0A,0
+	part_1_colon:           db "Part 1: ",0
+	newline_part_2_colon:   db 0x0A,"Part 2: ",0
+	newline:                db 0x0A,0
+section .bss
+	scoreboard: resb 8
--- common.asm
+++ common.asm
@@ -16,9 +16,27 @@ strlen: ; u64 strlen(char* str)
 	strlen_return:
 	ret
 
-error_and_exit: ; void error_and_exit(char* error_string)
-	; rdi already holds error string
+print: ; void print(char* str)
+	sub rsp, 0x10
+	mov QWORD [rsp], rdi
 	call strlen
+	mov rdi, QWORD [rsp]
+	add rsp, 0x10
+
+	mov rdx, rax      ; str length
+	mov rsi, rdi      ; str
+	mov rdi, 1        ; stdout
+	mov rax, 1        ; sys_write
+	syscall
+
+	ret
+
+error_and_exit: ; void error_and_exit(char* error_string)
+	sub rsp, 0x10
+	mov QWORD [rsp], rdi
+	call strlen
+	mov rdi, QWORD [rsp]
+	add rsp, 0x10
 
 	mov rdx, rax      ; error_string length	
 	mov rsi, rdi      ; error_string
@@ -97,11 +115,11 @@ section .text
 		cmp r11, 10
 		jae parse_u64_failed
 
-		imul rdx, 10                ; local_value *= 10
-		jb parse_u64_failed         ; fail on overflow
+		imul rdx, rdx, 10           ; local_value *= 10
+		jb parse_u64_failed         ; fail on overflow, imul sets carry flag which jb jumps on
 		
 		add rdx, r11                ; local_value += digit
-		jb parse_u64_failed         ; fail on overflow
+		jb parse_u64_failed         ; fail on overflow, unsigned so jump on carry instead of the overflow flag
 
 		inc r10
 		jmp parse_u64_loop
@@ -139,4 +157,42 @@ section .text
 		syscall
 
 		add rsp, 32
+		ret
+
+	quicksort_u64: ; void quicksort(u64* arr, u64 len)
+		cmp rsi, 1                       ; if (len <= 1) return;
+		ja partition
+		ret
+		partition:
+		mov r10, QWORD [rdi + rsi*8 - 8] ; pivot = arr[len - 1]
+		mov rdx, 0                       ; i = 0
+		mov rcx, 0                       ; j = 0
+		partition_loop:
+		cmp rdx, rsi                     ; for (; i < len; ++i)
+		jge partition_loop_end
+		mov r11, QWORD [rdi + rdx*8]     ; current = arr[i]
+		cmp r11, r10                     ; if (current <= pivot)
+		ja partition_loop_step
+		mov r8, QWORD [rdi + rcx*8]      ; tmp = arr[j]
+		mov QWORD [rdi + rcx*8], r11     ; arr[j] = current
+		mov QWORD [rdi + rdx*8], r8      ; arr[i] = tmp
+		inc rcx                          ; j += 1
+		partition_loop_step:
+		inc rdx
+		jmp partition_loop
+		partition_loop_end:
+		sub rsp, 0x20
+		mov QWORD [rsp + 24], rdi
+		mov QWORD [rsp + 16], rsi
+		mov QWORD [rsp +  8], rcx
+		mov rdi, rdi                     ; arr = &arr[0]
+		lea rsi, [rcx - 1]               ; len = j - 1
+		call quicksort_u64
+		mov rdi, QWORD [rsp + 24]
+		mov rsi, QWORD [rsp + 16]
+		mov rcx, QWORD [rsp +  8]
+		lea rdi, QWORD [rdi + rcx*8]     ; arr = &arr[j]
+		sub rsi, rcx                     ; len = len - j
+		call quicksort_u64
+		add rsp, 0x20
 		ret
